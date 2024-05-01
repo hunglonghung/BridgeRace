a61f3f3c207cb938f7c60431d747e32ce593795b
hunglonghung <emyeuanh18042004@gmail.com>
2024-05-01 16:09:29 +1000

    remake map

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public FloatingJoystick FloatingJoystick;
+    public Rigidbody rb;
+    public Animator anim;
+    public string currentAnimName;
+
+    public void FixedUpdate()
+    {
+        Debug.Log("Horizontal: " + FloatingJoystick.Horizontal + ", Vertical: " + FloatingJoystick.Vertical);
+        Vector3 direction = Vector3.forward * FloatingJoystick.Vertical + Vector3.right * FloatingJoystick.Horizontal;
+        rb.velocity = direction * speed;
+        if(FloatingJoystick.Horizontal != 0 || FloatingJoystick.Vertical !=0)
+        {
+            rb.velocity = direction * speed;
+            rb.rotation = Quaternion.LookRotation(direction);
+
+            ChangeAnim("running");
+            Debug.Log("Changed");
+        }
+        else if(FloatingJoystick.Horizontal == 0 && FloatingJoystick.Vertical ==0)
+        {
+            ChangeAnim("idle");
+        }
+    }
+
+    protected void ChangeAnim(string animName)
+    {
+        if (currentAnimName != animName)
+        {
+            anim.ResetTrigger(animName);
+            currentAnimName = animName;
+            anim.SetTrigger(currentAnimName);
+        }
+    }
+    
+}
