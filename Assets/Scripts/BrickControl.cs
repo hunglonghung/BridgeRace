55c9211865291843086f77b6d2138759f6253744
hunglonghung <emyeuanh18042004@gmail.com>
2024-05-02 11:33:05 +1000

    Still having bugs for brick build

--- Assets/Scripts/BrickControl.cs
+++ Assets/Scripts/BrickControl.cs
@@ -2,24 +2,41 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Scriptable;
+using Unity.VisualScripting;
 public class BrickControl : MonoBehaviour
 {
     [SerializeField] private GameObject objectToInstantiate;
     private GameObject block;
     [SerializeField] public int blockCount;
+    [SerializeField] private GameObject objectToFollow;
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "Block")
+        if(other.tag == "Block" )
         {
             AddBlock();
+            DeleteObject(other);
+        }
+    }
+
+    private void DeleteObject(Collider block)
+    {
+        // block.gameObject.SetActive(false);
+        Character characterComponent = block.GetComponent<Character>();
+        if (characterComponent != null)
+        {
+            characterComponent.ChangeColor(ColorType.None);
         }
     }
 
     private void AddBlock()
     {
         blockCount ++;
-        GameObject newBlock = Instantiate(objectToInstantiate, gameObject.transform.position, Quaternion.identity, gameObject.transform);
-        newBlock.transform.position = new Vector3(newBlock.transform.position.x + 0.3f * blockCount, newBlock.transform.position.y , newBlock.transform.position.z);
+        AddBlockOnPlayer();
+    }
+    private void AddBlockOnPlayer()
+    {
+        GameObject newBlock = Instantiate(objectToInstantiate, transform.position, Quaternion.identity,gameObject.transform);
+        newBlock.transform.position = objectToFollow.transform.position + Vector3.up * (float)(1f + blockCount * 0.5f) + Vector3.back * 1f;
     }
 }
